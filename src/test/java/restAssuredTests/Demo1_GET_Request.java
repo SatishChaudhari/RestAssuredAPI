373867710b54cf60e19ff453a114be8c7e25481d
Satesh <sateshchaudhari01@gmail.com>
2020-05-22 01:55:29 +0100

    Update

---
+++ src/test/java/restAssuredTests/Demo1_GET_Request.java
@@ -0,0 +1,35 @@
+package restAssuredTests;
+
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+/*
+Given()
+    set cookies, add authorisation, add param, set headers info etc..
+When()
+    GET, POST, PUT, DELETE....
+Then()
+    validate status code, extract response, extract headers cookies & response body....
+ */
+public class Demo1_GET_Request {
+    @Test
+    public void getProfileDetails(){
+        given()
+                .when()
+                .get("http://mcaindia.bananaapps.co.uk/api/user/Profile?userid=1326")
+                .then()
+                .statusCode(200)
+                .and()
+               .assertThat().body("Msg.Message", equalTo("User details retrieved successfully."))
+                .and()
+                .header("Content-Type", "application/json; charset=utf-8");
+
+
+    }
+
+
+
+}
